45a591501995c4f9d332505867d3a9766e157206
Zane Falcao <falcaozane@gmail.com>
2023-03-26 10:45:56 +0530

    Initial

---
+++ src/App.vue
@@ -0,0 +1,250 @@
+<script setup>
+  import {ref, shallowRef, computed, watch, nextTick} from 'vue'
+  import Chart from 'chart.js/auto'
+  import App from './App.vue'
+
+  const weights = ref([])
+
+  const weightChartEl = ref(null)
+
+  const weightChart = shallowRef(null)
+
+  const weightInput = ref()
+
+  const currentWeight = computed(()=>{
+    return weights.value.sort((a,b) => b.date - a.date)[0] || { weight: 0 }
+  })
+
+  const addWeight = () =>{
+    weights.value.push({
+        weight: weightInput.value,
+        date: new Date().getTime()
+    })
+  }
+
+  watch(weights, newWeights =>{
+    const ws = [...newWeights]
+
+    if (weightChart.value) {
+        weightChart.value.data.labels = ws
+          .sort((a,b)=> a.date - b.date)
+          .map(weight => new Date(weight.date).toLocaleDateString() )
+          .slice(-7)
+
+          weightChart.value.data.datasets[0].data = ws
+          .sort((a,b)=> a.date - b.date)
+          .map(weight => weight.weight)
+          .slice(-7)
+
+          weightChart.value.update()
+          return
+    }
+    nextTick(()=>{
+      weightChart.value = new Chart(weightChartEl.value.getContext('2d'), {
+        type:'line',
+        data:{
+          labels: ws
+                .sort((a,b)=> a.date - b.date)
+                .map(weight => new Date(weight.date).toLocaleDateString()),
+          datasets: [
+            {
+              label:'Weight',
+              data: ws
+              .sort((a, b)=> a.date - b.date)
+              .map(weight => weight.weight),
+              backgroundColor: 'rgba(80,120,250,0.3)',
+              borderColor: 'rgba(80, 120, 250,1)',
+              borderWidth:1,
+              fill: true
+            }
+          ]
+        },
+        options:{
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      })
+    })
+  }, {deep: true})
+</script>
+
+<template>
+    <main>
+      <h1>Weight Tracker</h1>
+
+      <div class="current">
+        <span>{{ currentWeight.weight }}</span>
+        <small>current weight in (kg)</small>
+      </div>
+
+      <form @submit.prevent="addWeight">
+          <input type="number" step="0.1" v-model="weightInput" placeholder="Enter weight in kg" />
+          <input type="submit" value="Add Weight" />
+      </form>
+      <div class="output" v-if="weights && weights.length >0">
+          <h2>Last 7 days</h2>
+          <div class="canvas-box">
+              <canvas ref="weightChartEl"></canvas>
+          </div>
+
+          <div class="weight-history">
+            <h2>Weight History</h2>
+            <ul>
+                <li v-for="weight in weights">
+                  <span>{{ weight.weight }}kg </span>
+                  <small>{{ new Date(weight.date).toLocaleDateString() }}</small>
+                </li>
+            </ul>
+          </div>
+      </div>
+    </main>
+</template>
+
+<style >
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'montserrat', sans-serif;
+}
+body {
+	background-color: #eee;
+}
+main {
+	padding: 1.5rem;
+}
+h1 {
+	font-size: 2em;
+	text-align: center;
+	margin-bottom: 2rem;
+}
+h2 {
+	margin-bottom: 1rem;
+	color: #888;
+	font-weight: 400;
+}
+.current {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 200px;
+	height: 200px;
+	
+	text-align: center;
+	background-color: white;
+	border-radius: 999px;
+	box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+	border: 5px solid hwb(253 9% 13%);
+	
+	margin: 0 auto 2rem;
+}
+.current:hover{
+  width: 250px;
+	height: 250px;
+  border: 5px solid hwb(253 37% 7%);
+}
+.current span {
+	display: block;
+	font-size: 2em;
+	font-weight: bold;
+	margin-bottom: 0.5rem;
+}
+.current small {
+	color: #888;
+	font-style: normal;
+}
+form {
+	display: flex;
+	margin-bottom: 2rem;
+	border: 1px solid #AAA;
+	border-radius: 0.5rem;
+	overflow: hidden;
+	transition: 200ms linear;
+}
+form:focus-within,
+form:hover {
+	border-color: blue;
+	border-width: 2px;
+}
+form input[type="number"] {
+	appearance: none;
+	outline: none;
+	border: none;
+	background-color: white;
+	flex: 1 1 0%;
+	padding: 1rem 1.5rem;
+	font-size: 1.25rem;
+}
+form input[type="submit"] {
+	appearance: none;
+	outline: none;
+	border: none;
+	cursor: pointer;
+	background-color: blue;
+	padding: 0.5rem 1.5rem;
+	color: white;
+	font-size: 1.25rem;
+	font-weight: 700;
+	transition: 200ms linear;
+	border-left: 3px solid transparent;
+}
+form input[type="submit"]:hover {
+	background-color: white;
+	color: blue;
+	border-left-color: blue;
+}
+.canvas-box {
+	width: 100%;
+	max-width: 820px;
+	background-color: white;
+	padding: 1.5rem;
+	border-radius: 0.5rem;
+	box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+	margin-bottom: 2rem;
+}
+.weight-history ul {
+	list-style: none;
+	padding: 0;
+	margin: 0;
+}
+.weight-history ul li {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0.5rem;
+	cursor: pointer;
+}
+.weight-history ul li:nth-child(even) {
+	background-color: #dfdfdf;
+}
+.weight-history ul li:hover {
+	background-color: #f8f8f8;
+}
+.weight-history ul li:last-of-type {
+	border-bottom: none;
+}
+.weight-history ul li span {
+	display: block;
+	font-size: 1.25rem;
+	font-weight: 700;
+	margin-right: 1rem;
+}
+.weight-history ul li small {
+	color: #888;
+	font-style: italic;
+}
+
+@media (max-width : 389px) {
+  .form input[type="number"]{
+      width: 50%;
+	    padding: 0;
+  }
+
+  .form input[type="submit"]{
+    padding: 0;
+  }
+}
+</style>
+
+
